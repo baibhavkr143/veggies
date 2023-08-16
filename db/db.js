ba617634d8552851d94a3d4368c85bf2d7a7a3ca
baibhav <baibhavkr143@gmail.com>
2023-08-17 01:13:16 +0530

    initial

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const bcrypt=require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 
-mongoose.connect("mongodb://localhost:27017/Veggies", { useNewUrlParser: true }).then(() => {
+mongoose.connect("mongodb://0.0.0.0:27017/Veggies", { useNewUrlParser: true}).then(() => {
     console.log("connection sucessful");
 }).catch((err) => {
     console.log(err);
