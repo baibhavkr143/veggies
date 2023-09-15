fce5903f62004731c15590c6f61ef497b36b6231
baibhav <baibhavkr143@gmail.com>
2023-09-15 14:06:04 +0530

    Remove config.env from tracking

--- router/payment_gateway.js
+++ router/payment_gateway.js
@@ -4,21 +4,36 @@ env.config({ path: "../backend/config.env" });
 const stripe= require('stripe')(process.env.secret_key);
 const router=express.Router();
 //code for accepting payment using stripe
-router.post('/customer/paymet', async (req, res) => {
-    try {
-      const { amount, currency } = req.body;
-  
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount,
-        currency,
-      });
-  
-      res.status(200).json({ clientSecret: paymentIntent.client_secret });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to create Payment Intent' });
-    }
-  });
+router.post("/customer/create-checkout-session", async (req, res) => {
+  const { total_price, currency } = req.body;
 
+  try {
+    const params = {
+      submit_type: 'pay',
+      mode: "payment",
+      payment_method_types: ['card'],
+      billing_address_collection: "auto",
+      line_items: [
+        {
+          price_data: {
+            currency: currency,
+            unit_amount: total_price * 100, // Convert to cents
+          },
+          quantity: 1, // Assuming you want to charge for one item
+        }
+      ],
+      success_url: `http://localhost:3000/success`,
+      cancel_url: `http://localhost:3000/cart`,
+    };
+
+    // Create the Stripe Checkout session
+    const session = await stripe.checkout.sessions.create(params);
+
+    res.json({ sessionId: session.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to create Checkout Session' });
+  }
+});
   module.exports=router;
   
